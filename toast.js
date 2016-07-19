a5c9f6c123e899ea94c9fa9694ec22f79cd5019b
v_liusimin <v_liusimin@baidu.com>
2016-07-19 15:14:42 +0800

    toast v1.0

---
+++ toast.js
@@ -0,0 +1,38 @@
+function toast(a){
+	if(!a){
+		return
+	}else if(document.getElementById('toats')){
+		return
+	}
+
+	var ToatsDom = document.createElement("div"); 
+		ToatsDom.innerHTML = a;
+		ToatsDom.id = 'toats';
+		ToatsDom.style.position = 'fixed';
+		ToatsDom.style.top = '50%';
+		ToatsDom.style.left = '50%';
+		ToatsDom.style.opacity = '0.8';
+		ToatsDom.style.background = '#000';
+		ToatsDom.style.color = '#fff';
+		ToatsDom.style.padding = '10px';
+	var Bl = document.body.firstChild; 
+
+	document.body.insertBefore(ToatsDom,Bl)
+
+	var ThisDom = document.getElementById('toats');
+	var ThisDomWidth = ThisDom.offsetWidth;
+	var ToatsDomLeft = '-'+(ThisDomWidth/2)+'px';
+		ToatsDom.style.marginLeft = ToatsDomLeft;
+
+
+		setTimeout('remove()',3000)
+}
+
+ function remove(){
+	var ThisDom = document.getElementById('toats');
+	if (!ThisDom) {
+		return
+	}else{
+		ThisDom.parentNode.removeChild(ThisDom);
+	}
+ }
